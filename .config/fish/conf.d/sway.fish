9577bf6f734d8f1c60f2c5710a251d6a569fa76d
Tor Hovland <tor+github@hovland.co>
2021-12-24 23:08:32 +0100

    Lots more config.

---
+++ .config/fish/conf.d/sway.fish
@@ -0,0 +1,3 @@
+if [ (tty) = "/dev/tty1" ]
+    exec sway
+end
--- .config/nixpkgs/home.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  imports = [ ./configs/main.nix ./packages/main.nix ];
-  nixpkgs.overlays = [ (import ./overlays/main.nix) ];
-  home.keyboard.layout = "no";
-}
